56a4f2ebab96a193261f8677b2e1cb8603a7ec93
DarwinMelaya <116789003+DarwinMelaya@users.noreply.github.com>
2025-01-28 15:21:57 +0800

    Fix Home Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Layout from "./components/Layout";
-
+import LandingPage from "./Pages/LandingPage";
 function App() {
   return (
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/feedback" element={<Home />} />
-          <Route path="/contact" element={<Home />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Layout>
     </Router>
