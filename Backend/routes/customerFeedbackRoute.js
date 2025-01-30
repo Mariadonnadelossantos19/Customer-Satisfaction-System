3f0ea7ad30eabc07eff19c747278e276f1f499ce
Mawi <jardinianomarklouisse@gmail.com>
2025-01-30 10:26:35 +0800

    create delete and get routes for customers feedback

--- Backend/routes/customerFeedbackRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { submitCustomerFeedback } = require("../controllers/customerFeedback");
-
-
-router.post("/submit-feedback", submitCustomerFeedback);
-
-module.exports = router;
