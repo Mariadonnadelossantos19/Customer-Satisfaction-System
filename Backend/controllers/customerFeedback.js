04efcd9eb3702e08f7d7307892bf5730abea4316
Mawi <jardinianomarklouisse@gmail.com>
2025-01-30 10:13:27 +0800

    create a route or customers feedback

--- Backend/controllers/customerFeedback.js
+++ Backend/controllers/customerFeedback.js
@@ -20,7 +20,7 @@ const submitCustomerFeedback = async (req, res) => {
       otherEducation,
       service,
       customerEvaluation,
-      libraryUser,
+      libraryUser
     } = req.body;
 
     if (
@@ -32,7 +32,7 @@ const submitCustomerFeedback = async (req, res) => {
       !gender ||
       !age ||
       !educationLevel ||
-      !service ||
+      !service || 
       !customerEvaluation ||
       !libraryUser
     ) {
@@ -40,9 +40,7 @@ const submitCustomerFeedback = async (req, res) => {
     }
 
     if (educationLevel === "Others" && !otherEducation) {
-      return res
-        .status(400)
-        .json({ message: "Please specify other education level." });
+      return res.status(400).json({ message: "Please specify other education level." });
     }
 
     const customerProfile = new CustomerProfile({
@@ -61,7 +59,7 @@ const submitCustomerFeedback = async (req, res) => {
       otherEducation,
       service,
       customerEvaluation,
-      libraryUser,
+      libraryUser
     });
     await customerProfile.save();
 
@@ -75,24 +73,4 @@ const submitCustomerFeedback = async (req, res) => {
   }
 };
 
-const fetchHistory = async (req, res) => {
-  try {
-    const getallFeedback = await CustomerProfile.find();
-    res.status(200).json(getallFeedback);
-  } catch (error) {
-    console.error("Error fetching history:", error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-};
-
-const deleteAllFeedback = async (req, res) => {
-  try {
-    const result = await CustomerProfile.deleteMany({});
-    res.status(200).json({ message: "All feedback records deleted", deletedCount: result.deletedCount });
-  } catch (error) {
-    console.error("Error deleting feedback:", error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-};
-
-module.exports = { submitCustomerFeedback, fetchHistory , deleteAllFeedback};
+module.exports = { submitCustomerFeedback };
--- Backend/routes/customerFeedbackRoute.js
+++ Backend/routes/customerFeedbackRoute.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {
-  submitCustomerFeedback,
-  fetchHistory,
-  deleteAllFeedback,
-} = require("../controllers/customerFeedback");
+const { submitCustomerFeedback } = require("../controllers/customerFeedback");
 
-router.get("/get-history", fetchHistory);
+
 router.post("/submit-feedback", submitCustomerFeedback);
-router.delete("/delete-feedback", deleteAllFeedback);
 
 module.exports = router;
